c65ab428e3b2f5508ac15fd0d6329fd095413203
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-06-28 12:45:45 +0200

    todo

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Display todo list progress"""
+
+import json
+import requestsgit 
+import sys
+if __name__ == "__main__":
+    USER_URL = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    res = requests.get(USER_URL)
+    user = json.loads(res.text)
+    num = sys.argv[1]
+    TODO_URL = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
+    res = requests.get(TODO_URL)
+    todos = json.loads(res.text)
+
+    done=[]
+
+for todo in todo_response:
+    i=0
+    if todo['completed']:
+          done.append(i)
+    print("Employee {} is done with tasks({}/{}):".format(
+                                                          USER['name'],
+                                                          len(done),
+                                                          len(todo)))
